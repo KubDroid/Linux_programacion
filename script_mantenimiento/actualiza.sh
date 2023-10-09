1eff9fda3e0b103830e9645c717c824cecabd8b4
KubDroid <ale.cubillo@gmail.com>
2023-10-09 13:19:25 -0600

    update

--- script_mantenimiento/actualiza.sh
+++ script_mantenimiento/actualiza.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+clear
+setterm -background red
+
+if [[ "$(id -u)" != "0" ]]; then
+echo "ESTE SCRIPT DEBE SER EJECUTADO COMO ROOT"
+sleep 3
+clear
+else
+echo "ESTE SCRIPT SERA EJECUTADO COMO SUPERUSUARIO (ROOT)"
+sleep 3
+clear
+fi
+
 sudo apt update
 sudo apt upgrade
 sudo apt autoremove
